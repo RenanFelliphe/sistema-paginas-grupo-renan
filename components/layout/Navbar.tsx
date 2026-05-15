54c4646f9adf561e34c3f7ce07b394f42f9cf916
Renan Felliphe de Moura Diogo Silva <felliphemouraa@gmail.com>
2026-05-14 22:18:52 -0300

    feat: adiciona link Inicio na Navbar

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -12,9 +12,7 @@ import { cn } from "@/lib/utils";
  *       { href: "/sua-rota", label: "Nome da Página" },
  */
 const navLinks: { href: string; label: string }[] = [
-  // ====== INÍCIO DA ÁREA DE EDIÇÃO COLABORATIVA ======
-
-  // ====== FIM DA ÁREA DE EDIÇÃO COLABORATIVA ======
+  { href: '/', label: 'Inicio' },
 ];
 
 export default function Navbar() {
